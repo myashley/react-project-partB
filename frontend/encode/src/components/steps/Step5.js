7ff9e99133f6092aae2e897e53d8f88468d58f7a
Ashley <36495929+myashley@users.noreply.github.com>
2018-07-23 17:32:59 +1000

    Add step 5 content

--- frontend/encode/src/components/steps/Step5.js
+++ frontend/encode/src/components/steps/Step5.js
@@ -4,16 +4,150 @@ export default class Step5 extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      ...props.getData()
+    }
   }
 
-  componentDidMount() {}
-
-
   render() {
+
+    const { 
+      firstName,
+      lastName,
+      phoneNumber,
+      emailAddress,
+      location,
+      techStack,
+      currentJobTitle,
+      currentEmployer,
+      roleType,
+      responsibilities,
+      priority,
+      expectedJobTitle,
+      expectedCompany,
+      minSalary,
+      expectedRoleType,
+      contactSource
+    } = this.state
+
+    const myTechStack = () => {
+      techStack.map((tech) => {
+        const {content} = tech
+        return content
+      })
+    }
+
+    const myPriority = () => {
+      priority.map((priorityItem) => {
+        return priorityItem
+      })
+    }
+
+    const myExpectedCompany = () => {
+      expectedCompany.map((company) => {
+        return company
+      })
+    }
+
+
     return (
+
       <div className="step step5">
+
+        <h2>Review</h2>
+        <div className="flex-container">
+
+          <div className="flex-item displayBlock">
+                <div className="displayField">
+                  <p className="labelField">First Name</p>
+                  <p className="resultField">{firstName}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Last Name</p>
+                  <p className="resultField">{lastName}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Mobile Number</p>
+                  <p className="resultField">{phoneNumber}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Email</p>
+                  <p className="resultField">{emailAddress}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Location</p>
+                  <p className="resultField">{location}</p>
+                </div>
+          </div>
+          <div className="flex-item displayBlock">
+                <div className="displayField">
+                  <p className="labelField">Current Job Title</p>
+                  <p className="resultField">{currentJobTitle}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Current Employer</p>
+                  <p className="resultField">{currentEmployer}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Currently in</p>
+                  <p className="resultField">{roleType} Role</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Day-to-Day Responsibilities</p>
+                  <p className="resultField">{responsibilities}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Technologies</p>
+                  <p className="resultField">{myTechStack}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Reume (Optional)</p>
+                  <p className="resultField"></p>
+                </div>
+          </div>
+
+          <div className="flex-item displayBlock">
+                <div className="displayField">
+                  <p className="labelField">Order of Importance</p>
+                  <p className="resultField">{myPriority}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Type of roles like to be contacted about</p>
+                  <p className="resultField">{expectedJobTitle}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Type of company like to work in next</p>
+                  <p className="resultField">{myExpectedCompany}</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">Minimum salary expectation</p>
+                  <p className="resultField">${minSalary}K</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">You are looking for:</p>
+                  <p className="resultField">{expectedRoleType} Role</p>
+                </div>
+
+                <div className="displayField">
+                  <p className="labelField">How did you hear about Encode Talent Management?</p>
+                  <p className="resultField">{contactSource}</p>
+                </div>
+          </div>
         
+        </div>       
       </div>
     )
   }
